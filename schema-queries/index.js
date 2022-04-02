8ef62e98c7be604a97754e7228310ab447cbc2bf
eurico77 <euricomagalhaes61@gmail.com>
2022-04-02 14:26:39 -0300

    add first type

--- schema-queries/index.js
+++ schema-queries/index.js
@@ -1,20 +1,40 @@
 const { gql, ApolloServer } = require("apollo-server");
 
 const typeDefs = gql`
+
+# pontos de entrada para queries
   type Query {
-    hello: String
+    Users: [User]
+  }
+
+  type User {
+    id: ID!
+    name: String
+    email: String
+    password: Float
+
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    Users: () => [
+      {
+        id: "1",
+        name: "John Doe",
+        email: "eurico@gmail.com",
+        password: "123456"
+      },
+    ],
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  cacheControl: {
+    defaultMaxAge: 5,
+  },
 });
 
 server.listen().then(({ url }) => {
